80997ad2be5f0a87aaea4c11457ace6304e8c929
Axel <axelthescarecrow@gmail.com>
2024-12-02 22:04:45 +0100

    Day 2

--- 2024/axelcarl/target/debug/deps/axelcarl-020592f1150309a4.d
+++ 2024/axelcarl/target/debug/deps/axelcarl-020592f1150309a4.d
@@ -1,6 +1,7 @@
-/Users/axelcarlsson/Projects/advent-of-code/2024/axelcarl/target/debug/deps/libaxelcarl-020592f1150309a4.rmeta: src/main.rs src/day1.rs
+/Users/axelcarlsson/Projects/advent-of-code/2024/axelcarl/target/debug/deps/libaxelcarl-020592f1150309a4.rmeta: src/main.rs src/day1.rs src/day2.rs
 
-/Users/axelcarlsson/Projects/advent-of-code/2024/axelcarl/target/debug/deps/axelcarl-020592f1150309a4.d: src/main.rs src/day1.rs
+/Users/axelcarlsson/Projects/advent-of-code/2024/axelcarl/target/debug/deps/axelcarl-020592f1150309a4.d: src/main.rs src/day1.rs src/day2.rs
 
 src/main.rs:
 src/day1.rs:
+src/day2.rs:
